b9450edf9b51f0bbc54c8dcb60988da8f4386c7f
TomekKasprzycki <tomasz.kasprzycki@gmail.com>
2019-07-14 16:32:27 +0200

    dodano userdao i db

---
+++ src/pl/coderslab/warsztat2krkw03/dao/UserDao.java
@@ -0,0 +1,39 @@
+package pl.coderslab.warsztat2krkw03.dao;
+
+import pl.coderslab.warsztat2krkw03.db.db;
+import pl.coderslab.warsztat2krkw03.model.User;
+
+import java.sql.*;
+
+public class UserDao {
+
+
+    private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?,?,?)";
+
+    public static User create(User user) {
+
+        try (Connection connection =
+                     DriverManager.getConnection(db.URL, db.USER, db.PASSWORD)
+        ) {
+
+            PreparedStatement ps = connection.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, user.getUsername());
+            ps.setString(2,user.getEmail());
+            ps.setString(3,user.getPassword());
+
+            ps.executeUpdate();
+
+            final ResultSet generatedKeys = ps.getGeneratedKeys();
+            if (generatedKeys.next()){
+                user.setId(generatedKeys.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+}
---
+++ src/pl/coderslab/warsztat2krkw03/db/db.java
@@ -0,0 +1,9 @@
+package pl.coderslab.warsztat2krkw03.db;
+
+public class db {
+    public static final String URL =
+            "jdbc:mysql://localhost:3306/warsztat2krkw03?useSSL=false&characterEncoding=utf8&serverTimezone=CET";
+    public static final String USER = "root";
+    public static final String PASSWORD = "coderslab";
+
+}
